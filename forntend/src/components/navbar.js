fd4e4d9e899c60ff5a92c5bfe107e8c63e7f4332
unknown <mohamedelrefaay11@icloud.com>
2023-06-15 00:54:36 +0300

    'refaay3'

---
+++ forntend/src/components/navbar.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+
+class Navbar extends Component{
+    render(){
+        return(
+            <div>
+<div className="container-fluid py-2 border-bottom d-none d-lg-block">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 text-center text-lg-start mb-2 mb-lg-0">
+                    <div className="d-inline-flex align-items-center">
+                        <a className="text-decoration-none text-body pe-3" href="/"><i className="bi bi-telephone me-2"></i>+0111 054 3007</a>
+                        <span className="text-body">|</span>
+                        <a className="text-decoration-none text-body px-3" href="/"><i className="bi bi-envelope me-2"></i>Gad@gmail.com</a>
+                    </div>
+                </div>
+                <div className="col-md-6 text-center text-lg-end">
+                    <div className="d-inline-flex align-items-center">
+                        <a className="text-body px-2" href="/">
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+                        <a className="text-body px-2" href="/">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a className="text-body px-2" href="/">
+                            <i className="fab fa-linkedin-in"></i>
+                        </a>
+                        <a className="text-body px-2" href="/">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                        <a className="text-body ps-2" href="/">
+                            <i className="fab fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+
+
+   
+    <div className="container-fluid sticky-top bg-white shadow-sm">
+        <div className="container">
+            <nav className="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
+                <a href="/" className="navbar-brand">
+                    <h1 className="m-0 text-uppercase text-primary"><i className="fa fa-clinic-medical me-2"></i>CMS</h1>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav ms-auto py-0">
+                        <a href="/" className="nav-item nav-link active">Home</a>
+                        <a href="/About" className="nav-item nav-link">About</a>
+                        <a href="/Service" className="nav-item nav-link">Service</a>
+                        <a href="/Price" className="nav-item nav-link">Pricing</a>
+                        <div className="nav-item dropdown">
+                            <a href="/" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div className="dropdown-menu m-0">
+                            <a href="Team" className="dropdown-item">The Team</a>
+                                <a href="/Testimonial" className="dropdown-item">The Patients Reviews</a>
+                                <a href="/Appointment" className="dropdown-item">Appointment</a>
+                                <a href="/Search" className="dropdown-item">Search</a>
+                            </div>
+                        </div>
+                        <a href="/Contact" className="nav-item nav-link">Contact</a>
+                        <a href="/Login" className="nav-item nav-link">Login</a>
+                        <a href="/Register" className="nav-item nav-link">Sign Up</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+   
+
+            </div>
+        )}}
+export default Navbar
--- forntend/src/components/testimonial.js
+++ forntend/src/components/testimonial.js
@@ -1,75 +1,13 @@
 import React, { Component } from "react";
+import Navbar from "./navbar";
+import Footer from "./footer";
  class Testimonial extends Component{
     render(){
         return(
             <div>
-{/* <!-- Topbar Start --> */}
-    <div class="container-fluid py-2 border-bottom d-none d-lg-block">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center text-lg-start mb-2 mb-lg-0">
-                    <div class="d-inline-flex align-items-center">
-                        <a class="text-decoration-none text-body pe-3" href="/Testimonial"><i class="bi bi-telephone me-2"></i>+0111 054 3007</a>
-                        <span class="text-body">|</span>
-                        <a class="text-decoration-none text-body px-3" href="/Testimonial"><i class="bi bi-envelope me-2"></i>Gad@gmail.com</a>
-                    </div>
-                </div>
-                <div class="col-md-6 text-center text-lg-end">
-                    <div class="d-inline-flex align-items-center">
-                        <a class="text-body px-2" href="/Testimonial">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a class="text-body px-2" href="/Testimonial">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a class="text-body px-2" href="/Testimonial">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                        <a class="text-body px-2" href="/Testimonial">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                        <a class="text-body ps-2" href="/Testimonial">
-                            <i class="fab fa-youtube"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-   {/*  <!-- Topbar End -->
 
+<Navbar/>
 
-    <!-- Navbar Start --> */}
-    <div class="container-fluid sticky-top bg-white shadow-sm">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
-                <a href="/" class="navbar-brand">
-                    <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>CMS</h1>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <div class="navbar-nav ms-auto py-0">
-                        <a href="/" class="nav-item nav-link active">Home</a>
-                        <a href="/About" class="nav-item nav-link">About</a>
-                        <a href="/Service" class="nav-item nav-link">Service</a>
-                        <a href="/Price" class="nav-item nav-link">Pricing</a>
-                        <div class="nav-item dropdown">
-                            <a href="/Testimonial" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                            <div class="dropdown-menu m-0">
-                                <a href="/Team" class="dropdown-item">The Team</a>
-                                <a href="/Testimonial" class="dropdown-item">The Patients Reviews</a>
-                                <a href="/Appointment" class="dropdown-item">Appointment</a>
-                                <a href="/Search" class="dropdown-item">Search</a>
-                            </div>
-                        </div>
-                        <a href="/Contact" class="nav-item nav-link">Contact</a>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </div>
    {/*  <!-- Navbar End -->
 
 
@@ -129,74 +67,7 @@ import React, { Component } from "react";
 
 
     <!-- Footer Start --> */}
-<div class="container-fluid bg-dark text-light mt-5 py-5">
-    <div class="container py-5">
-        <div class="row g-5">
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Get In Touch</h4>
-                <p class="mb-4">Visit us again</p>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>Assiut</p>
-                <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>medogadalrb@gmail.com</p>
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+0111 054 3007</p>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Quick Links</h4>
-                <div class="d-flex flex-column justify-content-start">
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Home</a>
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
-                    <a class="text-light" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Popular Links</h4>
-                <div class="d-flex flex-column justify-content-start">
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Home</a>
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                    <a class="text-light mb-2" href="Testimonial"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                    <a class="text-light mb-2" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
-                    <a class="text-light" href="/Testimonial"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Newsletter</h4>
-                <form action="">
-                    <div class="input-group">
-                        <input type="text" class="form-control p-3 border-0" placeholder="Your Email Address"/>
-                        <button class="btn btn-primary">Sign Up</button>
-                    </div>
-                </form>
-                <h6 class="text-primary text-uppercase mt-4 mb-3">Follow Us</h6>
-                <div class="d-flex">
-                    <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="/Testimonial"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="/Testimonial"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="/Testimonial"><i class="fab fa-linkedin-in"></i></a>
-                    <a class="btn btn-lg btn-primary btn-lg-square rounded-circle" href="/Testimonial"><i class="fab fa-instagram"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container-fluid bg-dark text-light border-top border-secondary py-4">
-    <div class="container">
-        <div class="row g-5">
-            <div class="col-md-6 text-center text-md-start">
-                <p class="mb-md-0">&copy; <a class="text-primary" href="/Testimonial">CMS</a>. All Rights Reserved.</p>
-            </div>
-            <div class="col-md-6 text-center text-md-end">
-                <p class="mb-0">Designed by <a class="text-primary" href="/Testimonial">GaAaD</a></p>
-            </div>
-        </div>
-    </div>
-</div>
-{/* <!-- Footer End -->
-
-
-    <!-- Back to Top --> */}
-    <a href="/Testimonial" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+<Footer/>
 
             </div>
         )}
