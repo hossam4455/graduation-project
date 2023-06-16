8ee5198bfb7cac99b3f795d87ffa133735f2643d
Hossam Hassan Mohamed Hassan <hossam.hassan@bld.ai>
2023-06-16 20:58:11 +0300

    frontedit

--- forntend/src/components/register.js
+++ forntend/src/components/register.js
@@ -1,35 +1,158 @@
-import React, { Component } from "react";
-import Navbar from "./navbar";
-import Footer from "./footer";
+import React, { useState } from 'react';
+import Navbar from './navbar';
+import Footer from './footer';
+import './css/css.css';
 
+const MyForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password1: '',
+    password2: '',
+    doctor_name: '',
+    department: '',
+    cost: '',
+    address: '',
 
- class Register extends Component{
-  
-    render(){
-        return(
-            <div>
-              <Navbar/>
-   {/*  <!-- Navbar END --> */ }
-   <div className="con">
-    <div className="form-con">
-     
+    image: null,
+    role: '',
+  });
 
-      <form id="signupForm" className="sform">
-        <h2>Sign Up</h2>
-        <input type="text" id="signupName" placeholder="Full Name" required/>
-        <input type="email" id="signupEmail" placeholder="Email" required/>
-        <input type="password" id="signupPassword" placeholder="Password" required/>
-        <button type="submit">Sign up</button>
-      </form>
+  const handleChange = (e) => {
+    if (e.target.name === 'image') {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.files[0],
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    const formDataToSend = new FormData();
+    formDataToSend.append('username', formData.username);
+    formDataToSend.append('email', formData.email);
+    formDataToSend.append('password1', formData.password1);
+    formDataToSend.append('password2', formData.password2);
+    formDataToSend.append('doctor_name', formData.doctor_name);
+    formDataToSend.append('department', formData.department);
+    formDataToSend.append('cost', formData.cost);
+    formDataToSend.append('address', formData.address);
+    formDataToSend.append('image', formData.image);
+    formDataToSend.append('role', formData.role);
+    fetch('http://127.0.0.1:8000/api/register', {
+      method: 'POST',
+      body: formDataToSend,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <Navbar />
+
+      <form onSubmit={handleSubmit} className='formone'>
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password1"
+          value={formData.password1}
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <label>Confirm Password:</label>
+        <input
+          type="password"
+          name="password2"
+          value={formData.password2}
+          onChange={handleChange}
+          placeholder="Confirm Password"
+        />
+             <label>Name:</label>
+      <input
+        type="text"
+        name="doctor_name"
+        value={formData.doctor_name}
+        onChange={handleChange}
+        placeholder="Name"
+      />
+      <label>Department:</label>
+      <input
+        type="text"
+        name="department"
+        value={formData.department}
+        onChange={handleChange}
+        placeholder="Department"
+      />
+      <label>Cost:</label>
+      <input
+        type="text"
+        name="cost"
+        value={formData.cost}
+        onChange={handleChange}
+        placeholder="Cost"
+      />
+      <label>Address:</label>
+      <input
+        type="text"
+        name="address"
+        value={formData.address}
+        onChange={handleChange}
+        placeholder="Address"
+      />
+              <label>Role:</label>
+        <select
+          name="role"
+          value={formData.role}
+          onChange={handleChange}
+        >
+          <option value="">Select Role</option>
+          <option value="doctor">Doctor</option>
+          <option value="patient">Patient</option>
+        </select>
+
+        <button type="submit">Submit</button>
+      <label>Uplode Image:</label>
+      <input
+        type="file" // Set the input type to 'file'
+        name="image"
+        onChange={handleChange}
+      />
+      <br/>
+      <button type="submit">Add Doctor</button>
+    </form>
+    <Footer/>
     </div>
-  </div>
+  );
+};
 
-
-   {/*<!-- Footer Start --> */}
-<Footer/>
-            </div>
-
-
-        )}
- }
- export default Register
+export default MyForm;
