59cf1b05ec83f64cd079a811ef9b4eb39f7e48c0
Hossam Hassan Mohamed Hassan <hossam.hassan@bld.ai>
2023-06-19 01:16:09 +0300

    add-appoiment

--- forntend/src/components/appointment.js
+++ forntend/src/components/appointment.js
@@ -1,87 +1,87 @@
-import React, { Component } from "react";
-import Navbar from "./navbar";
-import Footer from "./footer";
-class Appointment extends Component{
-    render(){
-        return(
-            <div>
+import React, { useState, useEffect } from 'react';
+  
+const CreateAppointment = () => {
+    const storedUserId = localStorage.getItem('id');
+  const [formData, setFormData] = useState({
+    doctor: '',
+    patient: storedUserId,
+    appointment_datetime: ''
+  });
 
+  const [doctors, setDoctors] = useState([]);
+  const [patients, setPatients] = useState([]);
 
-<Navbar />
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/register')
+      .then(response => response.json())
+      .then(data => {
+        setDoctors(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-    {/*<!-- Appointment Start --> */}
-    <div className="container-fluid py-5">
-        <div className="container">
-            <div className="row gx-5">
-                <div className="col-lg-6 mb-5 mb-lg-0">
-                    <div className="mb-4">
-                        <h5 className="d-inline-block text-primary text-uppercase border-bottom border-5">Appointment</h5>
-                        <h1 className="display-4">Make An Appointment For Your Family</h1>
-                    </div>
-                    <p className="mb-5"></p>
-                    <a className="btn btn-primary rounded-pill py-3 px-5 me-3" href="/Search">Find Doctor</a>
-                    <a className="btn btn-outline-primary rounded-pill py-3 px-5" href="/About">Read More</a>
-                </div>
-                <div className="col-lg-6">
-                    <div className="bg-light text-center rounded p-5">
-                        <h1 className="mb-4">Book An Appointment</h1>
-                        <form>
-                            <div className="row g-3">
-                                <div className="col-12 col-sm-6">
-                                    <select className="form-select bg-white border-0" style={{height: "55px"}}>
-                                        <option selected>Choose Department</option>
-                                        <option value="1">Department 1</option>
-                                        <option value="2">Department 2</option>
-                                        <option value="3">Department 3</option>
-                                    </select>
-                                </div>
-                                <div className="col-12 col-sm-6">
-                                    <select className="form-select bg-white border-0" style={{height: "55px"}}>
-                                        <option selected>Select Doctor</option>
-                                        <option value="1">Doctor 1</option>
-                                        <option value="2">Doctor 2</option>
-                                        <option value="3">Doctor 3</option>
-                                    </select>
-                                </div>
-                                <div className="col-12 col-sm-6">
-                                    <input type="text" className="form-control bg-white border-0" placeholder="Your Name" style={{height: "55px"}}/>
-                                </div>
-                                <div className="col-12 col-sm-6">
-                                    <input type="email" className="form-control bg-white border-0" placeholder="Your Email" style={{height: "55px"}}/>
-                                </div>
-                                <div className="col-12 col-sm-6">
-                                    <div className="date" id="date" data-target-input="nearest">
-                                        <input type="text"
-                                            className="form-control bg-white border-0 datetimepicker-input"
-                                            placeholder="Date" data-target="#date" data-toggle="datetimepicker"style={{height: "55px"}}/>
-                                    </div>
-                                </div>
-                                <div className="col-12 col-sm-6">
-                                    <div className="time" id="time" data-target-input="nearest">
-                                        <input type="text"
-                                            className="form-control bg-white border-0 datetimepicker-input"
-                                            placeholder="Time" data-target="#time" data-toggle="datetimepicker" style={{height: "55px"}}/>
-                                    </div>
-                                </div>
-                                <div className="col-12">
-                                    <button className="btn btn-primary w-100 py-3" type="submit">Make An Appointment</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+    fetch('http://127.0.0.1:8000/api/register')
+      .then(response => response.json())
+      .then(data => {
+        setPatients(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://127.0.0.1:8000/appointments/appointments/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data as needed
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle the error
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Create Appointment</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="doctor">Doctor:</label>
+        <select id="doctor" name="doctor" onChange={handleInputChange}>
+          <option value="">Select a doctor</option>
+          {doctors.map(doctor => (
+            <option key={doctor.id} value={doctor.id}>{doctor.doctor_name}</option>
+          ))}
+        </select>
+
+        {/* <label htmlFor="patient">Patient:</label>
+        <select id="patient" name="patient" onChange={handleInputChange}>
+          <option value="">Select a patient</option>
+          {patients.map(patient => (
+            <option key={patient.id} value={patient.id}>{patient.doctor_name}</option>
+          ))}
+        </select> */}
+
+        <label htmlFor="appointment_datetime">Appointment Date and Time:</label>
+        <input type="datetime-local" id="appointment_datetime" name="appointment_datetime" onChange={handleInputChange} />
+
+        <button type="submit">Create</button>
+      </form>
     </div>
-   {/*  <!-- Appointment End -->
+  );
+};
 
-
-<!-- Footer Start --> */}
-
-<Footer />
-
-            </div>
-        )
-    }
-}
-export default Appointment
+export default CreateAppointment;
