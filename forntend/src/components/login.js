970d88db8129702daad03fca21c3fba7bdda22a1
Hossam Hassan Mohamed Hassan <hossam.hassan@bld.ai>
2023-06-21 22:55:14 +0300

    viewprofile

--- forntend/src/components/login.js
+++ forntend/src/components/login.js
@@ -3,6 +3,7 @@ import Navbar from './navbar';
 import Footer from './footer';
 import './css/css.css';
 import { useNavigate } from 'react-router-dom';
+
 const MyForm = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
@@ -15,6 +16,7 @@ const MyForm = () => {
     address: '',
     image: null,
   });
+  const [result, setResult] = useState('');
 
   const handleChange = (e) => {
     if (e.target.name === 'image') {
@@ -32,26 +34,11 @@ const MyForm = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    
+
     const loginData = {
       username: formData.username,
       password: formData.password,
     };
-    const isUserSignedIn = () => {
-      const token = localStorage.getItem('token');
-      
-      return !!token; // Returns true if token exists, false otherwise
-    };
-    
-    // Example usage
-    if (isUserSignedIn()) {
-      // User is signed in
-      console.log('User is signed in');
-    } else {
-      // User is not signed in
-      console.log('User is not signed in');
-    }
-    
 
     fetch('http://127.0.0.1:8000/api/login', {
       method: 'POST',
@@ -64,23 +51,22 @@ const MyForm = () => {
       .then((data) => {
         // Retrieve the token from the response
         const token = data.token;
-        const id=data.user.id
-        const email=data.user.email
+        const id = data.user.id;
+        const email = data.user.email;
         console.log(token);
         localStorage.setItem('token', token);
         localStorage.setItem('id', id);
         localStorage.setItem('email', email);
 
+        setResult('User is signed in');
         navigate('/');
         setTimeout(() => {
           window.location.reload();
-          }, 1000);
-
-        // You can save the token in local storage or state for future use
-        // For example: localStorage.setItem('token', token);
+        }, 1000);
       })
       .catch((error) => {
         console.error(error);
+        setResult('Login failed');
       });
   };
 
@@ -92,6 +78,7 @@ const MyForm = () => {
 
     // ...
 
+    setResult('Form submitted successfully');
   };
 
   return (
@@ -113,7 +100,6 @@ const MyForm = () => {
           name="password"
           value={formData.password}
           onChange={handleChange}
-
           placeholder="Password"
         />
         <button type="submit">Login</button>
@@ -121,7 +107,9 @@ const MyForm = () => {
 
       {/* The remaining code for the doctor form */}
       {/* ... */}
-      
+
+      <p>{result}</p>
+
       <Footer />
     </div>
   );
